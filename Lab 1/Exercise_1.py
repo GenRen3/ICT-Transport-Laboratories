99fb3a5945512ddf12189b27c4fc97a64e48b29b
Gennaro Rende <44288923+jackrende@users.noreply.github.com>
2019-10-27 15:01:07 +0100

    First part done

--- Lab 1/Exercise_1.py
+++ Lab 1/Exercise_1.py
@@ -1,4 +1,19 @@
-import numpy
-import pymongo
-from pymongo import MongoClient
-import matplotlib
+import pymongo as pm #pymongo
+import pprint as pp #pretty print
+
+#Here we set the client
+client = pm.MongoClient('bigdatadb.polito.it', ssl=True,
+         authSource = 'carsharing', tlsAllowInvalidCertificates=True)
+
+#Here we access to a specific collection of the client
+db = client['carsharing']
+db.authenticate('ictts', 'Ictts16!', mechanism='SCRAM-SHA-1') #authentication
+
+Bookings_collection = db['PermanentBookings'] #Collection for Car2go to use
+
+#Example of a json file of a car in Vancouver
+pp.pprint(Bookings_collection.find_one({"city":"Vancouver"}))
+
+#Here finds the city of Turin in a certain set of time
+Bookings_collection.find( {"city": "Torino"},{"init_time":1, "city":1, "_id":0,
+                           "init_date":1})
