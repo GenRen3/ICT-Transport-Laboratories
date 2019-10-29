e78d28fd3f4d3211eda5d3f3d334a014b3468a11
Gennaro Rende <44288923+jackrende@users.noreply.github.com>
2019-10-29 11:23:17 +0100

    Stage

--- Lab 1/Exercise_1.py
+++ Lab 1/Exercise_1.py
@@ -1,5 +1,5 @@
 import pymongo as pm
-import pprint as pp 
+import pprint as pp
 
 #Here we set the client
 client = pm.MongoClient('bigdatadb.polito.it', ssl=True,
@@ -10,11 +10,11 @@ db = client['carsharing']
 db.authenticate('ictts', 'Ictts16!', mechanism='SCRAM-SHA-1') #authentication
 
 #Collection for Car2go to use, this a cursor that I need to convert as a list (PANDAS)
-Bookings_collection = db['PermanentBookings'] 
+Bookings_collection = db['PermanentBookings']
 
 #Example of a json file of a car in Vancouver
 pp.pprint(Bookings_collection.find_one({"city":"Vancouver"}))
 
 #Here finds the city of Turin in a certain set of time
 Bookings_collection.find( {"city": "Torino"},{"init_time":1, "city":1, "_id":0,
-                           "init_date":1})
+                           "init_date":1}).pretty()
