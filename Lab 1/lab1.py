0c1448dfb5701432db71f9972a43cf37dcf42a40
GiuliaCiaramella <44870147+GiuliaCiaramella@users.noreply.github.com>
2019-10-29 17:13:43 +0100

    giulia version

---
+++ Lab 1/lab1.py
@@ -0,0 +1,25 @@
+import pymongo as pm
+
+# configuration
+client = pm.MongoClient('bigdatadb.polito.it', ssl=True, authSource = 'carsharing', tlsAllowInvalidCertificates=True)
+db = client['carsharing'] #Choose the database to use
+db.authenticate('ictts', 'Ictts16!')# , mechanism='MONGODB-CR')
+
+permanentBook = db['PermanentBookings']
+permanentPark = db['PermanentParkings']
+activeBook = db['ActiveBookings']
+activePark = db['ActiveParkings']
+
+enjoy_permanentBook = db['enjoy_PermanentBookings']
+enjoy_permanentPark = db['enjoy_PermanentParkings']
+enjoy_activeBook = db['enjoy_ActiveBookings']
+enjoy_activePark = db['enjoy_ActiveBookings']
+
+ict_PermanentBook = db['ictts_PermanentBookings']
+ict_enjoy_PermanentBook = db['ictts_enjoy_PermanentBookings']
+
+
+query = {"city":"Torino", "init_fuel" : {"$gt":0}}
+print("Here!")
+result = permanentBook.find(query).count(True) # searching for vehicles in Turin with fuel greater than 14
+print(result)
